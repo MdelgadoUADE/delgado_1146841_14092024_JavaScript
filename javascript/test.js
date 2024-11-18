e48feaaa769fd507cf89de8a0f65c935da026271
Matías Delgado <156246262+VistaO7@users.noreply.github.com>
2024-11-18 15:13:17 -0300

    first commit

---
+++ javascript/test.js
@@ -0,0 +1,13 @@
+let nombre = prompt("Ingrese su nombre: ");
+let apellido = prompt("Ingrese su apellido: ");
+let edad = prompt("Ingrese su edad: ");
+
+alert(
+  "Hola " +
+    nombre +
+    " " +
+    apellido +
+    " cumpliste " +
+    edad * 365 +
+    " días de vida!"
+);
